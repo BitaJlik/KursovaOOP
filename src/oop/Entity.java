13a3497c456dc48947cc0f89fa0b25b742f49948
BitaJlik <vtkach4488@gmail.com>
2021-03-23 00:21:02 +0200

    Trying add Base "Home"

--- src/oop/Entity.java
+++ src/oop/Entity.java
@@ -1,41 +1,44 @@
 package oop;
 
-public class Entity {
+public class Entity implements Cloneable {
     private String name;
     private int id ;
     private double hp;
     private double damage;
+    private double x;
+    private double y;
+    private double speed;
     //--------------------
     public void Attack(){}
     public void Healing(double heal){}
     public void Death(){}
     public void Sleeping(){}
+    //------------------------------
+    public  void moveUp(){ }
+    public  void moveDown(){ }
+    public  void moveLeft(){ }
+    public  void moveRight(){ }
     //------------------------------ Геттери \ Сеттери
-    public String getName() {
-        return name;
-    }
+    public double getX() { return x; }
+    public void setX(double x) { this.x = x; }
+    //
+    public double getY() { return y; }
+    public void setY(double y) { this.y = y; }
+    //------------------
+    public double getSpeed() { return speed; }
+    public void setSpeed(double speed) { this.speed = speed; }
+    //------------------
+    public String getName() { return name; }
     public void setName(String name){}
     //-----------------
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
     //-----------------
-    public double getHp() {
-        return hp;
-    }
-    public void setHp(double hp) {
-        this.hp = hp;
-    }
+    public double getHp() { return hp; }
+    public void setHp(double hp) { this.hp = hp; }
     //------------------
-    public double getDamage() {
-        return damage;
-    }
-    public void setDamage(double damage) {
-        this.damage = damage;
-    }
+    public double getDamage() { return damage; }
+    public void setDamage(double damage) { this.damage = damage; }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/oop/Main.java
+++ src/oop/Main.java
@@ -3,21 +3,22 @@ package oop;
 import java.util.Scanner;
 
 import static oop.MainMenu.menu_entity_ch;
+import oop.Home;
 
 public class Main {
-
+    static int SizeMap = 500;
     static Scanner sc = new Scanner(System.in);
     static int size = 10;
-
     static Entity[] entity = new Entity[size];
-
     public static void main(String[] args) {
 
         for(int i = 0;i<10;i++)        //---------- Створюємо ліст Об'єктів
             entity[i] = new Spider();
-        entity[1] = new Steve(); // Добавляємо Стіва
 
+
+        entity[1] = new Steve("Гоша",20,2,50,24,10); // Добавляємо Стіва
         System.out.println("\n\nВиберіть :");
+
         MainMenu.SeeEntity(); // Початковий список
         menu_entity_ch = sc.nextInt();
 
