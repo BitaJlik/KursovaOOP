10ce49507dbd8a33ac2732e5299b12d8205f763c
BitaJlik <vtkach4488@gmail.com>
2021-05-01 23:16:47 +0300

    Making inventory

--- src/oop/Items/Ores.java
+++ src/oop/Items/Ores.java
@@ -9,19 +9,18 @@ public class Ores extends Items{
       super(name,price,amount,digSeconds);
     }
     public Ores(){ this("Неизвестно",0,-1,1); }// If object is not correct
- //   @Override
-//    public String toString(){
-//        return "Руда  "+ super.getName() + " | " +"\u001B[32m"+ super.getPrice() +"\u001B[0m" +"$ ("+ super.getAmount() + ")шт.";
-//    }
+    @Override
+    public String toString(){
+        return "Руда  "+ super.getName() + " | " +"\u001B[32m"+ super.getPrice() +"\u001B[0m" +"$ ("+ super.getAmount() + ")шт.";
+    }
     public static void oresinit(){
         ores.add( new Ores("Алмаз",10,5,1));
         ores.add( new Ores("Золото",8,5,1));
         ores.add( new Ores("Залізо",4,3,1));
         ores.add( new Ores("Свинець",4,3,1));
-        ores.add( new Ores("Мідь",4,3,1));
+        ores.add( new Ores("Мідь",3,3,1));
     }
     public Items clone(){
-        Items clone = super.clone();
-        return clone;
+        return new Ores(super.getName(),super.getPrice(),super.getDigseconds(),super.getAmount());
     }
 }
